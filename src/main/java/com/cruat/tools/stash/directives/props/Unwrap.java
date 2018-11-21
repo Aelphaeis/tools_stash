930cd46cd586582605b35f782275d6b1bf0783ab
Joseph Morain <morain@echoworx.com>
2018-11-21 12:19:49 -0500

    Turn Unwrap to instructionDirective

--- src/main/java/com/cruat/tools/stash/directives/props/Unwrap.java
+++ src/main/java/com/cruat/tools/stash/directives/props/Unwrap.java
@@ -1,12 +1,18 @@
 package com.cruat.tools.stash.directives.props;
 
+import com.cruat.tools.stash.config.Instruction;
 import com.cruat.tools.stash.directives.Directive;
+import com.cruat.tools.stash.directives.InstructionDirective;
 import com.cruat.tools.stash.directives.Supports;
 import com.cruat.tools.stash.transformer.PropertyTransformer;
 
 @Supports({ PropertyTransformer.class })
-public class Unwrap implements Directive{
-	public Unwrap() {
-		//TODO implement
+public class Unwrap implements InstructionDirective, Directive{
+
+	@Override
+	public Instruction execute(Instruction i) {
+		String value = i.getValue();
+		value = value.substring(1, value.length() - 1);
+		return new Instruction(i.getKey(), value, i.getLocation());
 	}
 }
