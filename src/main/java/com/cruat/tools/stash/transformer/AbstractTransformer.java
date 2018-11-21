7579bf0a6808f37e789a9c8b2a04b00e4beeb08e
Joseph Morain <morain@echoworx.com>
2018-11-21 12:25:00 -0500

    Refactor location of process instruction directive

--- src/main/java/com/cruat/tools/stash/transformer/AbstractTransformer.java
+++ src/main/java/com/cruat/tools/stash/transformer/AbstractTransformer.java
@@ -1,5 +1,7 @@
 package com.cruat.tools.stash.transformer;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,18 +10,23 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
+import java.util.function.BinaryOperator;
 import java.util.Optional;
 
 import com.cruat.tools.stash.config.Instruction;
 import com.cruat.tools.stash.directives.Directive;
+import com.cruat.tools.stash.directives.InstructionDirective;
 import com.cruat.tools.stash.exceptions.AggregationException;
 import com.cruat.tools.stash.exceptions.StashException;
 import com.cruat.tools.stash.exceptions.StashRuntimeException;
+import com.cruat.tools.stash.utils.IgnoreKeyCaseMap;
 
 public abstract class AbstractTransformer implements Transformer {
 	Set<Directive> directives;
@@ -71,12 +78,44 @@ public abstract class AbstractTransformer implements Transformer {
 			Instruction inst = entry.getValue();
 			try {
 				validateDirectives(inst);
+				entry.setValue(processInstructionDirective(inst));
 			}
 			catch(StashException e) {
 				throw new StashRuntimeException(e);
 			}
 		}
 		transformInternal(kvp);
+	}
+	
+	private Instruction processInstructionDirective(Instruction inst) {
+		List<String> dirs = inst.getDirectives();
+		Map<String, InstructionDirective> applicable = getDirectives().stream()
+				.filter(InstructionDirective.class::isInstance)
+				.map(InstructionDirective.class::cast)
+				.map(p -> new SimpleEntry<>(p.getName().toLowerCase(), p))
+				.filter(p -> dirs.contains(p.getKey()))
+				.collect(toMap(SimpleEntry::getKey, SimpleEntry::getValue,
+						throwingMerger(),
+						() -> ignoreKeyCaseMap(new HashMap<>())));
+
+		Instruction result = inst;
+		for(String dirName : dirs) {
+			InstructionDirective directive = applicable.get(dirName);
+			if(directive != null) {
+				result = directive.execute(result);
+			}
+		}
+		return result;
+	}
+
+	private static <T> BinaryOperator<T> throwingMerger(){
+        return (u,v) -> {
+        	String err = String.format("Duplicate key %s", u);
+        	throw new IllegalStateException(err);
+    	};
+	}
+	private static <E> Map<String, E> ignoreKeyCaseMap(Map<String, E> m){
+		return new IgnoreKeyCaseMap<>(m);
 	}
 	
 	public abstract void transformInternal(Map<String, Instruction> kvp);
--- src/main/java/com/cruat/tools/stash/transformer/PropertyTransformer.java
+++ src/main/java/com/cruat/tools/stash/transformer/PropertyTransformer.java
@@ -1,19 +1,13 @@
 package com.cruat.tools.stash.transformer;
 
-import static java.util.stream.Collectors.toMap;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.function.BinaryOperator;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -23,9 +17,7 @@ import org.apache.logging.log4j.Logger;
 
 import com.cruat.tools.stash.config.Instruction;
 import com.cruat.tools.stash.directives.Directive;
-import com.cruat.tools.stash.directives.InstructionDirective;
 import com.cruat.tools.stash.exceptions.StashException;
-import com.cruat.tools.stash.utils.IgnoreKeyCaseMap;
 
 public class PropertyTransformer extends AbstractTransformer {
 	private static final Logger logger = LogManager.getLogger();
@@ -56,10 +48,7 @@ public class PropertyTransformer extends AbstractTransformer {
 	public void transformInternal(Map<String, Instruction> kvp) {
 		for (Entry<String, Instruction> entry : kvp.entrySet()) {
 			String key = entry.getKey();
-			
 			Instruction inst = entry.getValue();
-			inst = processInstructionDirective(inst);
-			
 			config.setProperty(key, inst.getValue());
 			logger.trace("modifying {} to {}", key, inst.getValue());
 		}
@@ -75,36 +64,7 @@ public class PropertyTransformer extends AbstractTransformer {
 		}
 	}
 
-	private Instruction processInstructionDirective(Instruction inst) {
-		List<String> directives = inst.getDirectives();
-		Map<String, InstructionDirective> applicable = getDirectives().stream()
-				.filter(InstructionDirective.class::isInstance)
-				.map(InstructionDirective.class::cast)
-				.map(p -> new SimpleEntry<>(p.getName().toLowerCase(), p))
-				.filter(p -> directives.contains(p.getKey()))
-				.collect(toMap(SimpleEntry::getKey, SimpleEntry::getValue,
-						throwingMerger(),
-						() -> ignoreKeyCaseMap(new HashMap<>())));
 
-		Instruction result = inst;
-		for(String dirName : directives) {
-			InstructionDirective directive = applicable.get(dirName);
-			if(directive != null) {
-				result = directive.execute(result);
-			}
-		}
-		return result;
-	}
-
-	private static <T> BinaryOperator<T> throwingMerger(){
-        return (u,v) -> {
-        	String err = String.format("Duplicate key %s", u);
-        	throw new IllegalStateException(err);
-    	};
-	}
-	private static <E> Map<String, E> ignoreKeyCaseMap(Map<String, E> m){
-		return new IgnoreKeyCaseMap<>(m);
-	}
 	
 	@Override
 	public void validateDirectivesInternal(Instruction i)
