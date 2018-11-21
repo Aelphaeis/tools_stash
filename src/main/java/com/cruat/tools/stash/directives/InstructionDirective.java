b335d1f4c79728a11742a1a891ba667e0acd45f2
Joseph Morain <morain@echoworx.com>
2018-11-21 11:46:17 -0500

    modify instruction directive interface

--- src/main/java/com/cruat/tools/stash/directives/InstructionDirective.java
+++ src/main/java/com/cruat/tools/stash/directives/InstructionDirective.java
@@ -3,5 +3,5 @@ package com.cruat.tools.stash.directives;
 import com.cruat.tools.stash.config.Instruction;
 
 public interface InstructionDirective extends Directive {
-	void execute(Instruction i);
+	Instruction execute(Instruction i);
 }
