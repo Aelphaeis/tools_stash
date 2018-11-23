56c1b09632c934ba6a126261bc4d9815454b24d4
Joseph Morain <morain@echoworx.com>
2018-11-23 17:24:12 -0500

    change transform signature

--- src/main/java/com/cruat/tools/stash/directives/xml/Remove.java
+++ src/main/java/com/cruat/tools/stash/directives/xml/Remove.java
@@ -3,15 +3,14 @@ package com.cruat.tools.stash.directives.xml;
 import com.cruat.tools.stash.config.Instruction;
 import com.cruat.tools.stash.directives.Supports;
 import com.cruat.tools.stash.directives.TransformDirective;
+import com.cruat.tools.stash.transformer.Transformer;
 import com.cruat.tools.stash.transformer.XmlTransformer;
 
 @Supports({ XmlTransformer.class })
 public class Remove implements TransformDirective{
-
 	@Override
-	public void execute(Instruction i) {
+	public void execute(Instruction i, Transformer t) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
