b46b0343565a8080f3e2d581a280b16154096f6c
Joseph Morain <morain@echoworx.com>
2019-07-04 15:34:12 -0400

    init implementation of default transformer.

--- src/main/java/stash/transformer/DefaultTransformer.java
+++ src/main/java/stash/transformer/DefaultTransformer.java
@@ -1,46 +1,76 @@
 package stash.transformer;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import stash.config.Instruction;
 import stash.exceptions.StashException;
 
 public class DefaultTransformer extends AbstractTransformer {
+	
+	private static final Logger logger = LogManager.getLogger();
+	private static final String NL = System.lineSeparator();
+	
+	private String content;
+	
+	public DefaultTransformer() {
+		super(TransformerFactory.CACHE.get(DefaultTransformer.class));
+	}
 
 	@Override
 	public void load(InputStream stream) throws IOException {
-		try(BufferedReader reader = newBufferedReader(stream)){
-			
+		List<String> lines = new ArrayList<>();
+		try(BufferedReader r = newBufferedReader(stream)){
+			for(String l = r.readLine(); l != null; l = r.readLine()) {
+				lines.add(l);
+			}
+			content = lines.stream().collect(Collectors.joining(NL));
 		}
+	}
+
+	@Override
+	public void save(OutputStream stream) throws IOException {
+		try(BufferedWriter w = newBufferedWriter(stream)){
+			w.write(content);
+		}
+	}
+
+	@Override
+	public void transformInternal(Map<String, Instruction> kvp) {
+		logger.entry(kvp);
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void validateDirectivesInternal(Instruction i)
+			throws StashException {
+		logger.entry(i);
+		throw new UnsupportedOperationException();
 	}
 	
 	private BufferedReader newBufferedReader(InputStream is) {
 		Reader temp = new InputStreamReader(is, StandardCharsets.UTF_8);
 		return new BufferedReader(temp);
 	}
-
-	@Override
-	public void save(OutputStream stream) throws IOException {
-		// TODO Auto-generated method stub
-		
+	
+	private BufferedWriter newBufferedWriter(OutputStream os) {
+		Writer temp = new OutputStreamWriter(os);
+		return new BufferedWriter(temp);
 	}
-
-	@Override
-	public void transformInternal(Map<String, Instruction> kvp) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void validateDirectivesInternal(Instruction i)
-			throws StashException {
-		// TODO Auto-generated method stub
-	}
+	
 }
