8a67dc21d440e7ec0346f50dcda418b5054566ff
Joseph Morain <morain@echoworx.com>
2019-07-04 15:44:08 -0400

    tests for default transformer.

--- src/test/java/stash/transformer/DefaultTransformerTest.java
+++ src/test/java/stash/transformer/DefaultTransformerTest.java
@@ -1,16 +1,28 @@
 package stash.transformer;
 
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class DefaultTransformerTest {
+	private static final String RES_DIR = "src/test/resources/";
+	private static final String TEST_FILE = "valid.log";
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+
+	private DefaultTransformer t;
+	
+	@Before
+	public void setup() {
+		t = new DefaultTransformer();
 	}
 	
 	@Test
-	public void test() {
+	public void load_ValidFile_contentLoaded() throws IOException {
+		t.load(new File(RES_DIR + TEST_FILE));
+		assertEquals("DefaultTransformer Test File", t.getContent());
 	}
-	
 }
